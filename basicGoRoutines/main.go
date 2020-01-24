0a7d67a0b523fad85d36397d36ed63212df1c7c5
Eric Njogu <kunadawa@gmail.com>
2020-01-24 12:41:28 +0300

    lesson: putting each goroutine to sleep, the other gets a chance to run

--- basicGoRoutines/main.go
+++ basicGoRoutines/main.go
@@ -5,15 +5,18 @@ import (
 )
 
 func main() {
+  dur10ms, _ := time.ParseDuration("10ms")
   go func () {
     for i := 0; i < 100; i++ {
       println("hello")
+      time.Sleep(dur10ms)
     }
   }()
 
   go func() {
     for i := 0; i < 100; i++ {
       println("go")
+      time.Sleep(dur10ms)
     }
   }()
 
